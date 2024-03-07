de9127d189b0bb23fac349e1d18467c1722b9c41
Emil <ekorzybski@gmail.com>
2024-03-07 21:51:43 +0100

    fix for CORS related issues

--- src/main/java/com/korzybskiemil/localgems/corsconfig/CorsConfig.java
+++ src/main/java/com/korzybskiemil/localgems/corsconfig/CorsConfig.java
@@ -2,35 +2,22 @@ package com.korzybskiemil.localgems.corsconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
 
+
     @Bean
-    @Profile("!local")
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
+    public WebMvcConfigurer localCorsConfigurer() {
+        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
             }
         };
     }
-
-    @Bean
-    @Profile("local")
-    public WebMvcConfigurer localCorsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                // Nie dodajemy żadnych ustawień dla lokalnego profilu
-            }
-        };
-    }
-
 
 
 }
